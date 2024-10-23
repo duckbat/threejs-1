434a5578f9dc13cef550aac0c679320b389a908b
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-10-23 14:36:18 +0300

    Updates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from 'https://cdn.skypack.dev/three';
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
 
