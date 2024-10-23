f6ba1071ee2d5448120f42f04574fd6d2248b203
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-10-23 14:33:55 +0300

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
